5f8e48f0ae14b072c508b3c994be9e3b6f36ece7
AlexBanning <90031850+AlexBanning@users.noreply.github.com>
2023-07-13 14:58:05 +0200

    Schedule-Script for FIFA WWC

---
+++ Script_Learning/FIFA_WWC_Schedule.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import gspread
+
+
+# Create a schedule for FIFA WWC
+with open('C:\\Users\\alexa\\PycharmProjects\\Work_Tracab\\FIFA-Gamestats\\tournamentInfo\\women_schedule.xml') as fp:
+    data = BeautifulSoup(fp, 'xml')
+
+rounds = data.find_all('tournament-round')
+fixtures = data.find_all("sports-event")
+
+
+league = 'FIFA WWC'
+# Create empty DF
+season = pd.DataFrame(columns=["Matchday", "MatchID", "KickOff", "Home", "Away", "League", "Stadium"])
+# Get info for all matches and update DF
+for j, round in enumerate(rounds):
+    matchday = round['round-key']
+    fixtures = round.find_all("sports-event")
+    for i, match in enumerate(fixtures):
+        date = match.find('event-metadata')["start-date-time"][0:10]
+        time = match.find('event-metadata')["start-date-time"][11:16]
+        ko_date = (datetime.strptime(date + ' ' + time, '%Y-%m-%d %H:%M') + timedelta(hours=2)).strftime('%Y-%m-%d %H:%M')
+        home = match.find_all('team')[0].find('team-metadata').find('name')["nickname"].encode("latin").decode("utf-8")
+        away = match.find_all('team')[1].find('team-metadata').find('name')["nickname"].encode("latin").decode("utf-8")
+        match_id = match.find('event-metadata')["event-key"]
+        stadium = match.find('event-metadata').find('site').find('site-metadata').find('name')['full'].encode("latin").decode("utf-8")
+
+        match_info = {"Matchday": matchday, "MatchID": match_id, "KickOff": ko_date, "Home": home, "Away": away,
+                      "League": league, "Stadium": stadium}
+
+        season = season.append(pd.DataFrame([match_info]))
+
+# Push the schedule to the schedule sheet
+gc = gspread.oauth(credentials_filename=
+                   'C:\\Users\\alexa\\PycharmProjects\\Work_Tracab\\Script_Learning\\google_creds.json'
+                   )
+
+dispo_sheet = gc.open_by_key("14Dx1un2S9USaZX_5OyL2JALvxW4Fg18_OzJXaSuwYmc")
+worksheet = dispo_sheet.worksheet("FIFA WWC")
+worksheet.update([season.columns.values.tolist()] + season.values.tolist())
