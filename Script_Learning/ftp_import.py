4dff1e063a3716272eea34cf81fca26eb8ffea81
AlexBanning <banningalexander@web.de>
2023-07-17 13:59:39 +0200

    Tests to import data from ftp directly

---
+++ Script_Learning/ftp_import.py
@@ -0,0 +1,34 @@
+from ftplib import FTP
+import ftputil
+
+# ftp = FTP(host="213.168.127.130", user="AlexTest", passwd="RobberyandLahm5%")
+
+# ftp.retrlines('LIST')
+# ftp.cwd('deltatre')
+# ftp.retrlines('LIST')
+# ftp.cwd('MatchInfo')
+# ftp.retrlines('LIST')
+# ftp.cwd('51')
+# ftp.retrlines('LIST')
+# ftp.cwd('2022')
+# ftp.retrlines('LIST')
+# ftp.cwd('schedule')
+# ftp.retrlines('LIST')
+
+# localfile = open(filename, 'wb')
+# ftp.retrbinary(cmd='RETR ' + filename, callback=localfile.write)
+
+# Different option to download files from FTP using FTPutil
+# Need to test if it also works to simply open a file and read the xml-information without downloading it
+server = "213.168.127.130"
+user = "AlexTest"
+password = "RobberyandLahm5%"
+filename = 'schedule.xml'
+f = open(filename, "w")
+with ftputil.FTPHost(server, user, password) as ftp_host:
+    ftp_host.chdir('deltatre/MatchInfo/51/2022/schedule')
+    ftp_host.open(filename)
+    if ftp_host.path.isfile(filename):
+        ftp_host.download(filename, filename)
+
+
