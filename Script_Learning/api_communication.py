1a5068bfa6238aa50a80123bd7cbc0a24697fd60
AlexBanning <banningalexander@web.de>
2023-08-22 12:19:53 +0200

    API Module Update

--- Script_Learning/api_communication.py
+++ Script_Learning/api_communication.py
@@ -6,43 +6,13 @@ from requests.structures import CaseInsensitiveDict
 import TracabModules.apiFunctions as af
 
 
-login_url = 'https://stagingdata.voetbaldatacentre.nl/api/login'
+login_url = 'https://data.voetbaldatacentre.nl/api/login'
 creds = '{"username": "chryonhego@archimedict.nl", "password": "34$h$kKs8y9Gqadp"}'
-match_id = '161783'
-line_up_url = 'https://stagingdata.voetbaldatacentre.nl/bv/api/match_lineup/161783'
 token = af.get_token(login_url, creds)
-lineups = af.get_both_lineups(token, '161783')
+lineups = af.get_both_lineups(token, 'M335374348')
 
-
-
-home = pd.DataFrame([{'Player': x['firstName'] + ' ' + y['lastName'],
-      'jerseyNumber': int(z['matchShirtNumber'])} for x, y, z in
-        zip(lineups['homeTeam']['players'],lineups['homeTeam']['players'], lineups['homeTeam']['players']) if
-                     type(z['matchShirtNumber']) == int ]).sort_values(by=['jerseyNumber'],axis=0, ascending=True)
-
-away = pd.DataFrame([{'Player': x['firstName'] + ' ' + y['lastName'],
-      'jerseyNumber': int(z['matchShirtNumber'])} for x, y, z in
-        zip(lineups['awayTeam']['players'],lineups['awayTeam']['players'], lineups['awayTeam']['players']) if
-                     type(z['matchShirtNumber']) == int ]).sort_values(by=['jerseyNumber'],axis=0, ascending=True)
-
-with open('2291800Gamestats.xml') as fp:
-    data = BeautifulSoup(fp, features='xml')
-
-home_gs = pd.DataFrame(
-    [{'Player': x['sFirstName'].encode('latin').decode('utf-8') + ' ' + y['sLastName'].encode('latin').decode('utf-8'),
-      'jerseyNumber': int(z['iJerseyNo'])} for x, y, z in
-     zip(data.find('Hego').find_all('Team')[0].find('Roster').find_all('Player'),
-         data.find('Hego').find_all('Team')[0].find('Roster').find_all('Player'),
-         data.find('Hego').find_all('Team')[0].find('Roster').find_all('Player'))]
-).sort_values(by=['jerseyNumber'],axis=0, ascending=True)
-
-away_gs = pd.DataFrame(
-    [{'Player': x['sFirstName'].encode('latin').decode('utf-8') + ' ' + y['sLastName'].encode('latin').decode('utf-8'),
-      'jerseyNumber': int(z['iJerseyNo'])} for x, y, z in
-     zip(data.find('Hego').find_all('Team')[1].find('Roster').find_all('Player'),
-         data.find('Hego').find_all('Team')[1].find('Roster').find_all('Player'),
-         data.find('Hego').find_all('Team')[1].find('Roster').find_all('Player'))]
-).sort_values(by=['jerseyNumber'],axis=0, ascending=True)
+test = pd.concat([lineups[0], lineups[1]], axis=1)
+print(lineups[0])
 
 # Tests with production environment
 
@@ -52,6 +22,43 @@ token = af.get_token(login_url, creds)
 headers = CaseInsensitiveDict()
 headers['accept'] = 'application/json'
 headers['Authorization'] = "Bearer " + token
-comp_info_url = 'https://data.voetbaldatacentre.nl/av/api/competitions'
-comp_info =  requests.get(comp_info_url, headers=headers).json()
 
+# Get general competition info
+comp_info_url = 'https://data.voetbaldatacentre.nl/av/api/competitions'
+comp_info = requests.get(comp_info_url, headers=headers).json()
+
+# Get matchinfo for all matches of current matchday (or of an previous MD using the calendar week
+matchday_url = 'https://data.voetbaldatacentre.nl/av/api/matches/33'
+md_info = requests.get(matchday_url, headers=headers).json()
+
+# Get lineups for a single match by match number
+lineup_url = 'https://data.voetbaldatacentre.nl/av/api/match/lineup/' + 'M335374348'
+lineup_info = requests.get(lineup_url, headers=headers).json()
+
+# Get all matches from a certain team
+team_match_url = 'https://data.voetbaldatacentre.nl/av/api/matches/team/' + str(md_info[1]['awayTeam']['teamId'])
+team_matches = requests.get(team_match_url, headers=headers).json()
+lineup_url = 'https://data.voetbaldatacentre.nl/av/api/match/lineup/' + str(team_matches[0]['matchNumber'])
+lineup_info = requests.get(lineup_url, headers=headers).json()
+
+
+matchday_url = 'https://data.voetbaldatacentre.nl/av/api/matches/33'
+md_info = requests.get(matchday_url, headers=headers).json()
+
+lineup_url = ['https://data.voetbaldatacentre.nl/av/api/match/lineup/' + str(x['matchNumber']) for x in md_info]
+lineup_info = [requests.get(x, headers=headers).json() for x in lineup_url]
+
+
+test = pd.DataFrame()
+for lineups in enumerate(lineup_info):
+    home = pd.DataFrame([{'Player': x['firstName'] + ' ' + y['lastName'],
+                          'jerseyNumber': int(z['matchShirtNumber'])} for x, y, z in
+                         zip(lineups[1]['homeTeam']['players'], lineups[1]['homeTeam']['players'],
+                             lineups[1]['homeTeam']['players'])])
+
+    away = pd.DataFrame([{'Player': x['firstName'] + ' ' + y['lastName'],
+                          'jerseyNumber': int(z['matchShirtNumber'])} for x, y, z in
+                         zip(lineups[1]['awayTeam']['players'], lineups[1]['awayTeam']['players'],
+                             lineups[1]['awayTeam']['players'])])
+
+    test = pd.concat([test, home, away])
