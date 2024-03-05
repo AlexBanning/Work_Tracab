3ca081a62bc6b4eda502b8aed92a9c92d01203a9
AlexBanning <banningalexander@web.de>
2024-03-05 11:46:29 +0100

    Adjusted file structure and deleted outdated scripts

---
+++ Schedule_Scripts/keytoq_schedules.py
@@ -0,0 +1,20 @@
+import TracabModules.scheduleFunctions as sf
+
+"""
+Schedule-Script for all competitions with keytoq as info provider
+"""
+
+# print('Which season do you want to update?')
+
+# season_id = input()
+comp_id = int(51)
+
+sf.get_schedule_xml(comp_id, vendor='keytoq')
+
+filename = 'main.xml'
+
+# Parse the schedule from xml-file to a pandas.Dataframe
+schedule = sf.get_keytoq_schedule(filename)
+
+# Push schedule to Google Sheets
+sf.push_to_google(schedule, league= schedule[5].iloc[0])
---
+++ dist/schedule.bat
@@ -0,0 +1,2 @@
+@echo off
+"N:\07_QC\Scripts\Schedule_script\Season23-24\all_schedules.exe"
