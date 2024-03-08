a6654822285063b0f8e0fd8a9fdb5497dc34de75
Alexander Banning <alexander.banning@tracab.com>
2024-03-08 11:54:11 +0100

    v1.0 of renameHTF.py

--- Workflow/renameHTF.py
+++ Workflow/renameHTF.py
@@ -1,17 +1,31 @@
 """
-Renaming the HTFs in the BL1 and BL2 requires:
-    - Knowing which game has been played
-    - The name/location of both HTFs that were generated
-        ~ HTF_1 always on D: and HTF_2 always on E: (EXCEPT AUGSBURG; CURRENTLY ONLY E AVAILABLE)
-    - STS-ID of the match
-        ~ Load in the sts file and fetch necessary information
-"""
+Purpose of this code is to find the latest HTF videos that were just rendered into .mp4 and then rename them according
+STS requirements.
 
+v1.0: 2024/03/08 11:45
+    - First version already able to rename the videos accordingly
+    - Currently this version needs to be adjusted based on bl1 or bl2 usage
+"""
 import pandas as pd
 import os
-import glob
-from TracabModules.Internal.server_manipulations import newest
-# bl1_schedule = pd.read_excel(r'C:\Users\a.banning\Desktop\BL1_HTF_Schedule.xlsx')
+from TracabModules.Internal.server_manipulations import newest_file
 
-list_of_files = glob.glob(r'C:/Users/a.banning/Desktop/*')
-latest_file = max(list_of_files, key=os.path.getctime)
+
+path_htf1 = r'D:\B'
+path_htf2 = r'E:\D'
+
+latest_htf1 = newest_file(path_htf1, 'mp4')
+latest_htf2 = newest_file(path_htf2, 'mp4')
+
+match = latest_htf1[-13:-6].replace('_', '-')
+print(match)
+
+bl2_schedule = pd.read_excel(r'C:\Users\tracab\Desktop\BL2_HTF_Schedule.xlsx', header=1)
+
+# Right/Left is seen from the benches => Default: left == HTF2
+new_htf2 = bl2_schedule.loc[bl2_schedule['3LC'] == match]['High Behind Left'].values[0]
+new_htf1 = bl2_schedule.loc[bl2_schedule['3LC'] == match]['High Behind Right'].values[0]
+
+
+os.rename(latest_htf2, path_htf2 + '\\' + new_htf2)
+os.rename(latest_htf1, path_htf1 + '\\' + new_htf1)
