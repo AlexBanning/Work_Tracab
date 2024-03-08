d017205f265e3f8582ebd459719b59530a711811
Alexander Banning <alexander.banning@tracab.com>
2024-03-08 15:16:29 +0100

    v1.1 of renameHTF and added special version for FCA. Added HTF-Schedule to fd_renameHTF.py

--- Workflow/filedownloader_renameHTF.py
+++ Workflow/filedownloader_renameHTF.py
@@ -4,12 +4,31 @@ server = "213.168.127.130"
 user = "Alex_Test"
 password = "RobberyandLahm5%"
 
+choice = input('For which league do you want to update the software? \n'
+               '1: BL1 \n'
+               '2: BL2 \n')
 
-filename = 'renameHTF_bl2.exe'
+filenames_bl2 = ['renameHTF_bl2.exe', 'BL2_HTF_Schedule.xlsx']
+filenames_bl1 = ['renameHTF_bl1.exe', 'BL1_HTF_Schedule.xlsx']
+
 ftp_dir = 'TRACAB_NEU/07_QC/Alex/Scripts/HTFs'
-
-with ftputil.FTPHost(server, user, password) as ftp_host:
-    ftp_host.chdir(ftp_dir)
-    ftp_host.open(filename)
-    if ftp_host.path.isfile(filename):
-        ftp_host.download(filename, filename)
+if choice == str(1):
+    for filename in filenames_bl1:
+        try:
+            with ftputil.FTPHost(server, user, password) as ftp_host:
+                ftp_host.chdir(ftp_dir)
+                ftp_host.open(filename)
+                if ftp_host.path.isfile(filename):
+                    ftp_host.download(filename, filename)
+        except:
+            pass
+elif choice == str(2):
+    for filename in filenames_bl2:
+        try:
+            with ftputil.FTPHost(server, user, password) as ftp_host:
+                ftp_host.chdir(ftp_dir)
+                ftp_host.open(filename)
+                if ftp_host.path.isfile(filename):
+                    ftp_host.download(filename, filename)
+        except:
+            pass
--- Workflow/renameHTF_FCA.py
+++ Workflow/renameHTF_FCA.py
@@ -1,13 +1,6 @@
 """
 Purpose of this code is to find the latest HTF videos that were just rendered into .mp4 and then rename them according
-STS requirements.
-
-v1.0: 2024/03/08 11:45
-    - First version already able to rename the videos accordingly
-    - Currently this version needs to be adjusted based on bl1 or bl2 usage
-    - SuperCup for BL1 schedule is neglected. NEEDS TO BE ADJUSTED FOR UPCOMING SEASON!!!
-v1.1: 2024/03/08 14:20
-    - Added additional check for the existence of both or only one HTF
+STS requirements. Adjusted version of renameHTF.py for the special set up in FCA with only one drive being available
 """
 import pandas as pd
 import os
@@ -18,7 +11,7 @@ choice = input('Are both HTFs available? Please provide the specific input and p
                '1: only HTF_1 (right) \n'
                '2: only HTF_2 (left) \n')
 
-path_htf1 = r'D:\B'
+path_htf1 = r'E:\B'
 path_htf2 = r'E:\D'
 
 latest_htf1 = newest_file(path_htf1, 'mp4')
@@ -26,12 +19,13 @@ latest_htf2 = newest_file(path_htf2, 'mp4')
 
 match = latest_htf1[-13:-6].replace('_', '-')
 
-bl2_schedule = pd.read_excel(r'C:\Users\tracab\Desktop\BL2_HTF_Schedule.xlsx', header=1)
-# Right/Left is seen from the benches => Default: left == HTF2
-new_htf2 = bl2_schedule.loc[bl2_schedule['3LC'] == match]['High Behind Left'].values[0]
-new_htf1 = bl2_schedule.loc[bl2_schedule['3LC'] == match]['High Behind Right'].values[0]
+bl1_schedule = pd.read_excel(r'C:\Users\tracab\Desktop\BL1_HTF_Schedule.xlsx', header=1)
 
-# Rename the available HTFs
+# Right/Left is seen from the benches => Default: left == HTF2
+
+new_htf2 = bl1_schedule.loc[bl1_schedule['3LC'] == match]['High Behind Left'].values[0]
+new_htf1 = bl1_schedule.loc[bl1_schedule['3LC'] == match]['High Behind Right'].values[0]
+
 if choice == str(0):
     os.rename(latest_htf2, path_htf2 + '\\' + new_htf2)
     os.rename(latest_htf1, path_htf1 + '\\' + new_htf1)
