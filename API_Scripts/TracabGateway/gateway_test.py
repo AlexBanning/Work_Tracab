6e3a84cec42d54da538f93c4a355770f95f98a78
AlexBanning <banningalexander@web.de>
2024-03-26 17:03:22 +0100

    some Gateway output testing and connecting to streamAPI

--- API_Scripts/TracabGateway/gateway_test.py
+++ API_Scripts/TracabGateway/gateway_test.py
@@ -1,26 +1,35 @@
 from requests.structures import CaseInsensitiveDict
 import requests
-import json
+
 import pandas as pd
 
+import socket
+import struct
+import json
 
 # Create header
 token = 'MGZhNjQ2ZTQ2NmQwOGFkNGE2NDYzMTlkNDFhN2FiNDUzZjgwZGIyYjhjNGNlNGMwODhmZDY1YjNmNjQ2YjdkZA=='
+user_id = '360'
 headers = CaseInsensitiveDict()
 headers['accept'] = 'application/json'
 headers['Authorization'] = 'Bearer ' + token
 
-
 # Define match parameters
-game_id = '2374331'
-vendor_id = '5'
+game_id = '13415'
+vendor_id = '10'
 extr_vers = '4'
-data_quality = '1'
+data_quality = '0'
+team_id = '215'
+
+# Call fixtures and results
+fixtures_url = f'https://api.tracab.com/api/fixtures/?VendorID={vendor_id}&Teams={team_id}'
+r_fixtures = requests.get(fixtures_url, headers=headers)
+fixtures = json.loads(r_fixtures.content.decode('utf8'))
 
 # Create metadata URL
-metadata_url = (f'https://api.tracab.com/api/V1/feeds/game_metadata?GameID={game_id}&VendorID={vendor_id}&ExtractionVersion={extr_vers}'
-                f'&DataQuality={data_quality}')
-
+metadata_url = (
+    f'https://api.tracab.com/api/V1/feeds/game_metadata?GameID={game_id}&VendorID={vendor_id}&ExtractionVersion={extr_vers}'
+    f'&DataQuality={data_quality}')
 
 # Send request to API
 r_meta = requests.get(metadata_url, headers=headers).json()
@@ -41,60 +50,138 @@ dfs = [pd.DataFrame(fr) for fr in player_frames]
 bin_url = f'https://api.tracab.com/api/V1/downloads/tbf?GameID={game_id}&VendorID={vendor_id}&ExtractionVersion={extr_vers}&DataQuality={data_quality}&Phase=0'
 r_bin = requests.get(bin_url, headers=headers)
 
-
-# Get raw data in dat file (ASCII format)
-dat_url = f'https://api.tracab.com/api/v1/downloads/dat?VendorID=5&GameID=2374331&DataQuality=0&ExtractionVersion=4'
+# Get raw data in dat file (ASCII format) and parse into seperate lists
+dat_url = (f'https://api.tracab.com/api/V1/downloads/dat?GameID={game_id}&VendorID={vendor_id}'
+           f'&ExtractionVersion={extr_vers}&DataQuality={data_quality}&Phase=0')
 r_dat = requests.get(dat_url, headers=headers)
 dat_text = r_dat.text
 # Split string into data parts (framecount:playerpositions:ballinfo)
 frames = dat_text.split(':')
-framecount = [x.replace('\n','') for x in frames[0::3]]
+framecount = [x.replace('\n', '') for x in frames[0::3]]
 del framecount[-1]
 # playerpositions
 player_frames = [x for x in frames[1::3]]
 players = [[y for y in x.split(';') if y != ''] for x in player_frames]
 players_split = [[x.split(',') for x in frame] for frame in players]
-x_pos_home = [[int(x[3]) for x in frame  if x[0] == '1'] for frame in players_split]
-x_pos_away = [[int(x[3]) for x in frame  if x[0] == '0'] for frame in players_split]
-x_pos_refs = [[int(x[3]) for x in frame  if x[0] == '3'] for frame in players_split]
-y_pos_home = [[int(x[4]) for x in frame  if x[0] == '1'] for frame in players_split]
-y_pos_away = [[int(x[4]) for x in frame  if x[0] == '0'] for frame in players_split]
-y_pos_refs = [[int(x[4]) for x in frame  if x[0] == '3'] for frame in players_split]
+x_pos_home = [[int(x[3]) for x in frame if x[0] == '1'] for frame in players_split]
+x_pos_away = [[int(x[3]) for x in frame if x[0] == '0'] for frame in players_split]
+x_pos_refs = [[int(x[3]) for x in frame if x[0] == '3'] for frame in players_split]
+y_pos_home = [[int(x[4]) for x in frame if x[0] == '1'] for frame in players_split]
+y_pos_away = [[int(x[4]) for x in frame if x[0] == '0'] for frame in players_split]
+y_pos_refs = [[int(x[4]) for x in frame if x[0] == '3'] for frame in players_split]
 
-columns_x = ['x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6', 'x_7', 'x_8', 'x_9', 'x_10', 'x_11']
-columns_y = ['y_1', 'y_2', 'y_3', 'y_4', 'y_5', 'y_6', 'y_7', 'y_8', 'y_9', 'y_10', 'y_11']
+# TF05 Feed (Heatmap and Formation)
+tf05_url = (f'https://api.tracab.com/api/V1/feeds/tf05?GameID={game_id}&VendorID={vendor_id}'
+            f'&ExtractionVersion={extr_vers}&DataQuality={data_quality}&Phase=0'
+            )
+r_tf05 = requests.get(tf05_url, headers=headers)
+tf05_data = json.loads(r_tf05.content.decode('utf8'))
 
-home_x = pd.DataFrame(x_pos_home, columns=columns_x)
-home_y = pd.DataFrame(y_pos_home, columns=columns_y)
+# TF09 Feed (Possession and Pass Data)
+tf09_url = (f'https://api.tracab.com/api/V1/feeds/tf09?GameID={game_id}&VendorID={vendor_id}'
+            f'&ExtractionVersion={extr_vers}&DataQuality={data_quality}'
+            )
+r_tf09 = requests.get(tf09_url, headers=headers)
+tf09_data = json.loads(r_tf09.content.decode('utf8'))
 
-# Initialize an empty list to store the merged columns
-merged_columns = []
+# TF08 (Physical Data)
+tf08_url = (f'https://api.tracab.com/api/V1/feeds/tf08?GameID={game_id}&VendorID={vendor_id}'
+            f'&ExtractionVersion={extr_vers}&DataQuality={data_quality}'
+            )
+r_tf08 = requests.get(tf08_url, headers=headers)
+tf08_data = json.loads(r_tf08.content.decode('utf8'))
 
-# Merge columns alternately
-for x_col, y_col in zip(columns_x, columns_y):
-    merged_columns.append(x_col)
-    merged_columns.append(y_col)
+# Get temporary API key for live subscription
+tmp_url = f'https://api.tracab.com/api/internal/generatetempapikey'
+r_tmpkey = requests.get(tmp_url, headers=headers)
+tmpkey = json.loads(r_tmpkey.content.decode('utf8'))['tempApiKey']
 
-# Select only the matching columns from home_x and home_y based on the merged pattern
-merged_home = pd.concat([home_x[columns_x], home_y[columns_y]], axis=1)
+# Define the JSON data for authentication
+auth_data = {
+    "TemporaryAPIKey": tmpkey,
+    "ClassID": "TCMDAuthenticationRequest",
+    "UserID": 360
+}
 
-# Rename columns based on the merged pattern
-merged_home.columns = merged_columns
+# Convert JSON data to string
+json_message = json.dumps(auth_data)
 
+# Calculate the size of the JSON message
+message_size = len(json_message)
 
-home_xy = [[(x,y) for x,y in zip(frame_x,frame_y)] for frame_x, frame_y in zip(x_pos_home,y_pos_home)]
-away_xy = [[(x,y) for x,y in zip(frame_x,frame_y)] for frame_x, frame_y in zip(x_pos_away,y_pos_away)]
-refs_xy = [[(x,y) for x,y in zip(frame_x,frame_y)] for frame_x, frame_y in zip(x_pos_refs,y_pos_refs)]
+# Convert the size to a 32-bit integer (4 bytes)
+size_bytes = struct.pack('!I', message_size)
 
-# DFs of all XY positions of all players and refs
-df_home = pd.DataFrame(home_xy)
-df_away = pd.DataFrame(away_xy)
-df_refs = pd.DataFrame(refs_xy)
+# Check connection with Heartbeat
+heartbeat_data = {
+    "ClassID": "TCMDClientHeartBeat",
+}
+# Convert JSON data to string
+heartbeat_message = json.dumps(heartbeat_data)
 
+# Calculate the size of the JSON message
+heartbeat_message_size = len(heartbeat_message)
 
-ball_frames = [x for x in frames[2::3]]
-# Create DF out of data lists
-dat_dict = {'framecount': framecount, 'player_frames': player_frames, 'ball_frames': ball_frames}
-dat_df = pd.DataFrame(dat_dict)
+# Convert the size to a 32-bit integer (4 bytes)
+heartbeat_size_bytes = struct.pack('!I', heartbeat_message_size)
 
+# Send StartSubscription
+startsub_data = {
+    "GetLive": False,
+    "GameID": 13415,
+    "ClassID": "TCMDStartGameStreamSubscriptionRequest",
+    "VendorID": 10,
+    "OutProtocol": "ASCII"
+}
+# Convert JSON data to string
+startsub_message = json.dumps(startsub_data)
 
+# Calculate the size of the JSON message
+startsub_message_size = len(startsub_message)
+
+# Convert the size to a 32-bit integer (4 bytes)
+startsub_size_bytes = struct.pack('!I', startsub_message_size)
+
+# Define the host and port of the stream API
+host = 'stream-api.tracab.com'
+port = 37718
+
+# Establish a connection to the stream API and send a heartbeat
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+
+    # Send the size of the JSON message as a 32-bit integer
+    s.sendall(size_bytes)
+
+    # Send the JSON message
+    s.sendall(json_message.encode())
+
+    # Receive response if needed
+    response = s.recv(1024)
+
+    # Process response
+    print("Response:", response.decode())
+
+    # Send the size of the JSON message as a 34-bit integer
+    s.sendall(heartbeat_size_bytes)
+
+    # Send the JSON message
+    s.sendall(heartbeat_message.encode())
+
+    # Receive response if needed
+    heartbeat_response = s.recv(1024)
+
+    # Process response
+    print("Response:", heartbeat_response.decode('iso-8859-1'))
+
+    # Send the size of the JSON message as a 32-bit integer
+    s.sendall(startsub_size_bytes)
+
+    # Send the JSON message
+    s.sendall(startsub_message.encode())
+
+    # Receive response if needed
+    startsub_response = s.recv(1024)
+
+    # Process response
+    print("Response:", startsub_response.decode('iso-8859-1'))
--- Script_Learning/reading_gw_output.py
+++ Script_Learning/reading_gw_output.py
@@ -1,7 +1,21 @@
 import json
-
+import requests
+import numpy as np
 import pandas as pd
+from requests.structures import CaseInsensitiveDict
 
+
+# Create header
+token = 'MGZhNjQ2ZTQ2NmQwOGFkNGE2NDYzMTlkNDFhN2FiNDUzZjgwZGIyYjhjNGNlNGMwODhmZDY1YjNmNjQ2YjdkZA=='
+headers = CaseInsensitiveDict()
+headers['accept'] = 'application/json'
+headers['Authorization'] = 'Bearer ' + token
+# Define match parameters
+game_id = '2374252'
+vendor_id = '5'
+extr_vers = '4'
+data_quality = '1'
+team_id = '215'
 
 '''
 JSON ------------------------------------------------------------------------------------------------------------------
@@ -27,3 +41,37 @@ home_pos = pd.DataFrame([[x['X'], y['Y']] for x,y in zip(example, example) if x[
 DAT-File ------------------------------------------------------------------------------------------------------------------
 '''
 df = pd.read_csv(r'C:\Users\a.banning\Downloads\2374244.dat', delimiter='\t')
+
+'''
+TF05-Feed -------------------------------------------------------------------------------------------------------------
+'''
+tf05_url = (f'https://api.tracab.com/api/V1/feeds/tf05?GameID={game_id}&VendorID={vendor_id}'
+            f'&ExtractionVersion={extr_vers}&DataQuality={data_quality}&Phase=0'
+            )
+r_tf05 = requests.get(tf05_url, headers=headers)
+tf05_file = json.loads(r_tf05.content.decode('utf8'))
+
+# Get heatmap data into a usable format
+num_rows = 14
+num_cols = 20
+heatmap_data = np.array([int(digit) for digit in tf05_file['HomeTeam']['Heatmap']]).reshape(num_rows, num_cols)
+
+'''
+TF09-Feed --------------------------------------------------------------------------------------------------------------
+'''
+# TF09 Feed (Possession and Pass Data)
+tf09_url = (f'https://api.tracab.com/api/V1/feeds/tf09?GameID={game_id}&VendorID={vendor_id}'
+            f'&ExtractionVersion={extr_vers}&DataQuality={data_quality}'
+            )
+r_tf09 = requests.get(tf09_url, headers=headers)
+tf09_data = json.loads(r_tf09.content.decode('utf8'))
+
+'''
+TF08-Feed --------------------------------------------------------------------------------------------------------------
+'''
+# TF08 (Physical Data)
+tf08_url = (f'https://api.tracab.com/api/V1/feeds/tf08?GameID={game_id}&VendorID={vendor_id}'
+            f'&ExtractionVersion={extr_vers}&DataQuality={data_quality}'
+            )
+r_tf08 = requests.get(tf08_url, headers=headers)
+tf08_data = json.loads(r_tf08.content.decode('utf8'))
