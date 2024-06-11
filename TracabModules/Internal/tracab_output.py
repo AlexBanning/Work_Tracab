d3cd88593556c3556bf49693751adfb003a728ac
AlexBanning <banningalexander@web.de>
2024-06-11 15:24:21 +0200

    Added eredivisie and ekstraklasa databases and made some functions more errorprone

--- TracabModules/Internal/tracab_output.py
+++ TracabModules/Internal/tracab_output.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from xml.dom.minidom import parse
-from pathlib import Path
+import logging
+from TracabModules.Internal.gamelog_functions import get_player_name
 
 bvb_validation_kpis = ["TeamID", "PlayerID", "PlayerNumber", "PlayerName",
                        "PlayTime (min)", "Distance (m)", "Distance/min",
@@ -15,6 +16,7 @@ bvb_validation_kpis = ["TeamID", "PlayerID", "PlayerNumber", "PlayerName",
                        "Distance IBC", "Sprints IBC", "SpeedRuns IBC",
                        "AvgTime IBC"]
 
+stats_report_kpis = ['Distance', 'Sprints', 'SpeedRuns']
 
 def write_excel(dfl_df, epl_df, match_info, obs):
     """
@@ -102,5 +104,74 @@ def get_observed_stats(report):
     # Extract stats for home and away teams
     home_stats = pd.DataFrame([extract_stats('HomeTeam')])
     away_stats = pd.DataFrame([extract_stats('AwayTeam')])
+
+    return home_stats, away_stats
+
+
+def get_validated_stats(filepath, gamelog_info):
+    logging.basicConfig(filename='stats_log.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+    files = [p for p in filepath.iterdir() if p.is_file() and 'Resolution.xml' in p.name]
+
+    players_data_home = []
+    players_data_away = []
+
+    # Loop through all PlayerID_Resolution.xml files to fetch KPI-scores for each player
+    for file in files:
+        try:
+            xml_doc = parse(str(file))
+            player_data_elements = xml_doc.getElementsByTagName('PlayerData')
+            if not player_data_elements:
+                logging.error(f'PlayerData not found in {file}')
+                continue
+            player_data = player_data_elements[0]
+            team_id = player_data.getAttribute('TeamID')
+            player_id = player_data.getAttribute('PlayerID')
+            player_name = get_player_name(str(gamelog_info['Gamelog']), team_id, player_id)
+
+            stats_elements = xml_doc.getElementsByTagName('Stats')
+            if not stats_elements:
+                logging.error(f'Stats not found in {file}')
+                continue
+            stats_element = stats_elements[-1]
+
+            player_og_stats = []
+            for kpi in stats_report_kpis:
+                kpi_elements = stats_element.getElementsByTagName(kpi)
+                if kpi_elements:
+                    player_og_stats.append(float(kpi_elements[0].childNodes[0].data))
+                else:
+                    player_og_stats.append(0.0)  # Default to 0 if KPI not found
+
+            if player_og_stats[0] == 0.0:
+                continue
+
+            player = {
+                "TeamID": team_id,
+                "PlayerID": player_id,
+                "PlayerName": player_name,
+                "Total Distance": player_og_stats[0],
+                "Num. Sprints": player_og_stats[1],
+                "Num. SpeedRuns": player_og_stats[2]
+            }
+
+            if team_id == gamelog_info['HomeId']:
+                players_data_home.append(player)
+            else:
+                players_data_away.append(player)
+
+        except Exception as e:
+            print(f"Error processing file {file}: {e}")
+            continue
+
+    def aggregate_stats(players_data):
+        if not players_data:
+            return pd.DataFrame()
+        df = pd.DataFrame(players_data)
+        totals = df[['Total Distance', 'Num. Sprints', 'Num. SpeedRuns']].sum().round(2)
+        return pd.DataFrame([totals])
+
+    home_stats = aggregate_stats(players_data_home)
+    away_stats = aggregate_stats(players_data_away)
 
     return home_stats, away_stats
