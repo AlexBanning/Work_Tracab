028eafb2d1672a5826c4abc266919e159dae8834
Alexander Banning <alexander.banning@tracab.com>
2024-08-29 16:28:02 +0200

    added logic to collect more pdf data from gateway output

--- Deliveries/Gateway/gateway_pdf.py
+++ Deliveries/Gateway/gateway_pdf.py
@@ -6,6 +6,7 @@ Generating a PDF out of Tracab's Gateway output.
 """
 import pandas as pd
 import json
+from collections import Counter
 from TracabModules.Internal.gateway import GatewayDownloader
 import numpy as np
 
@@ -91,7 +92,7 @@ for team in ['HomeTeam', 'AwayTeam']:
     n_hia = [x + y for x, y in zip(n_sprints, n_speedruns)]
     sprint_distance = [np.round(
         ((x['PercentDistanceHighSpeedSprinting'] + x['PercentDistanceLowSpeedSprinting']) / 100 * x['Distance']), 2) for
-                       x in ft[team]['Players']]
+        x in ft[team]['Players']]
     speedrun_distance = [np.round((x['PercentDistanceHighSpeedRunning'] / 100 * x['Distance']), 2) for x in
                          ft[team]['Players']]
     lowspeedrun_distance = [np.round((x['PercentDistanceLowSpeedRunning'] / 100 * x['Distance']), 2) for x in
@@ -216,11 +217,14 @@ for team in ['HomeTeam', 'AwayTeam']:
 """
 Summary: Ball Control
 """
+
+
 def calculate_sprint_distance(possession_data):
     high_speed = possession_data.get('PercentDistanceHighSpeedSprinting', 0)
     low_speed = possession_data.get('PercentDistanceLowSpeedSprinting', 0)
     distance = possession_data.get('Distance', 0)
     return np.round((high_speed + low_speed) * distance / 100, 2)
+
 
 ball_control_stats = {}
 for team in ['HomeTeam', 'AwayTeam']:
@@ -249,34 +253,179 @@ for team in ['HomeTeam', 'AwayTeam']:
         for x in ft[team]['Players']
     ]
     opo_sprint_dist = [
-         calculate_sprint_distance(x['PossessionData']['OpponentPossession'])
-         for x in ft[team]['Players']
-     ]
+        calculate_sprint_distance(x['PossessionData']['OpponentPossession'])
+        for x in ft[team]['Players']
+    ]
     db_sprint_dist = [
-         calculate_sprint_distance(x['PossessionData']['DeadBall'])
-         for x in ft[team]['Players']
-     ]
+        calculate_sprint_distance(x['PossessionData']['DeadBall'])
+        for x in ft[team]['Players']
+    ]
 
     own_hsr_dist = [np.round(x['PossessionData']['OwnTeamPossession']['PercentDistanceHighSpeedRunning'] *
-                                x['PossessionData']['OwnTeamPossession']['Distance'] / 100,2)
-                       for x in ft[team]['Players']]
-    opo_hsr_dist = [np.round(x['PossessionData']['OpponentPossession']['PercentDistanceHighSpeedRunning']*
-                                x['PossessionData']['OpponentPossession']['Distance'] / 100,2)
-                       for x in ft[team]['Players']]
-    db_hsr_dist = [np.round(x['PossessionData']['DeadBall']['PercentDistanceHighSpeedRunning']*
-                                x['PossessionData']['DeadBall']['Distance'] / 100,2)
-                       for x in ft[team]['Players']]
+                             x['PossessionData']['OwnTeamPossession']['Distance'] / 100, 2)
+                    for x in ft[team]['Players']]
+    opo_hsr_dist = [np.round(x['PossessionData']['OpponentPossession']['PercentDistanceHighSpeedRunning'] *
+                             x['PossessionData']['OpponentPossession']['Distance'] / 100, 2)
+                    for x in ft[team]['Players']]
+    db_hsr_dist = [np.round(x['PossessionData']['DeadBall']['PercentDistanceHighSpeedRunning'] *
+                            x['PossessionData']['DeadBall']['Distance'] / 100, 2)
+                   for x in ft[team]['Players']]
 
     own_hi_runs = [x['PossessionData']['OwnTeamPossession']['Sprints'] +
-               x['PossessionData']['OwnTeamPossession']['SpeedRuns']
-               for x in ft[team]['Players']]
+                   x['PossessionData']['OwnTeamPossession']['SpeedRuns']
+                   for x in ft[team]['Players']]
     opo_hi_runs = [x['PossessionData']['OpponentPossession']['Sprints'] +
-               x['PossessionData']['OpponentPossession']['SpeedRuns']
-               for x in ft[team]['Players']]
+                   x['PossessionData']['OpponentPossession']['SpeedRuns']
+                   for x in ft[team]['Players']]
     db_hi_runs = [x['PossessionData']['DeadBall']['Sprints'] +
-               x['PossessionData']['DeadBall']['SpeedRuns']
-               for x in ft[team]['Players']]
+                  x['PossessionData']['DeadBall']['SpeedRuns']
+                  for x in ft[team]['Players']]
 
-    sprint_distance = [np.round(
-        ((x['PercentDistanceHighSpeedSprinting'] + x['PercentDistanceLowSpeedSprinting']) / 100 * x['Distance']), 2) for
-                       x in ft[team]['Players']]
+    team_dict = {x: {'mins_playerd': np.round(m, 2),
+                     'Total dist': t, 'Sprint dist': s, 'HSR dist': h, 'High Int. Runs': i,
+                     'Total dist Opo': ot, 'Sprint dist Opo': os, 'HSR dist Opo': oh, 'High Int. Runs Opo': oi,
+                     'Total dist DB': dt, 'Sprint dist DB': ds, 'HSR dist DB': dh, 'High Int. Runs DB': di}
+                 for x, m, t, s, h, i, ot, os, oh, oi, dt, ds, dh, di in
+                 zip(players, mins_played, own_dist, own_sprint_dist, own_hsr_dist, own_hi_runs,
+                     opo_dist, opo_sprint_dist, opo_hsr_dist, opo_hi_runs,
+                     db_dist, db_sprint_dist, db_hsr_dist, db_hi_runs)}
+
+    ball_control_stats.update({team: team_dict})
+
+"""
+Pass summary
+"""
+
+for team in ['HomeTeam', 'AwayTeam']:
+    players = [
+        f"{name_parts[0][0]}. {' '.join(name_parts[1:])}" if len(name_parts) > 1 else name_parts[0]
+        for x in ft[team]['Players']
+        if (name_parts := x['PlayerName'].split())
+    ]
+
+    passes_count = [
+        np.round(sum(1
+                     for x in y['SuccessfulPasses']), 2)
+        for y in tf09_data[team]['Players']
+    ]
+
+    max_index, max_value = max(enumerate(passes_count),
+                               key=lambda x: x[1])  # Only returns one value, what if several players with equal amount?
+    most_passes_player = players[max_index]
+
+    passes_count_total = sum(passes_count)
+
+    pass_pressured_count = [
+        np.round(sum(1
+                     for x in y['SuccessfulPasses']
+                     if x['ReleasePressure'] == 'HighPressure'), 2)
+        for y in tf09_data[team]['Players']
+    ]
+
+    max_index_pressure, max_value_pressure = max(enumerate(pass_pressured_count), key=lambda x: x[
+        1])  # Only returns one value, what if several players with equal amount?
+    most_passes_pressure_player = players[max_index_pressure]
+
+    pass_pressured_count_total = sum(pass_pressured_count)
+    pass_pressured_perc = np.round(pass_pressured_count_total / passes_count_total * 100, 2)
+
+    bypassed_count = [
+        np.round(sum(x['BypassedPlayers']['Count']
+                     for x in y['SuccessfulPasses']), 2)
+        for y in tf09_data[team]['Players']
+    ]
+    bypassed_count_total = sum(bypassed_count)
+
+    recipients = [
+        Counter([x['ReceiveNumber']
+                 for x in y['SuccessfulPasses']])
+        for y in tf09_data[team]['Players']
+    ]
+
+    numbers = [
+        x['JerseyNo']
+        for x in metadata[team]['Players']
+    ]
+
+    pass_matrix = pd.DataFrame(0, index=numbers, columns=numbers)
+    # Populate the DataFrame with counts from recipients_count
+    for sender_idx, count in enumerate(recipients):
+        sender_number = numbers[sender_idx]  # Get the sender's jersey number
+        for receiver_number, cnt in count.items():
+            pass_matrix.at[sender_number, receiver_number] = cnt  # Fill in the pass count
+
+    # Display the resulting DataFrame
+    print(pass_matrix)
+
+    # Release/Receive Pressure from/in Location --- SKIPPED UNTIL ACTUAL INTEREST
+    # Attacking 3rd:
+
+"""
+Passing Successful
+"""
+players = [
+    f"{name_parts[0][0]}. {' '.join(name_parts[1:])}" if len(name_parts) > 1 else name_parts[0]
+    for x in ft[team]['Players']
+    if (name_parts := x['PlayerName'].split())
+]
+
+sp_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['FromSetPiece'] is True), 2)
+    for y in tf09_data[team]['Players']
+]
+open_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['FromSetPiece'] is False), 2)
+    for y in tf09_data[team]['Players']
+]
+
+off_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['Direction'] is 'Offensive'), 2)
+    for y in tf09_data[team]['Players']
+]
+
+def_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['Direction'] is 'Defensive'), 2)
+    for y in tf09_data[team]['Players']
+]
+
+byp_count = [
+    np.round(sum(x['BypassedPlayers']['Count']
+                 for x in y['SuccessfulPasses']), 2)
+    for y in tf09_data[team]['Players']
+]
+
+high_press_rec_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['ReceiverPressure'] == 'HighPressure'), 2)
+    for y in tf09_data[team]['Players']
+]
+low_press_rec_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['ReceiverPressure'] == 'LowPressure'), 2)
+    for y in tf09_data[team]['Players']
+]
+no_press_rec_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['ReceiverPressure'] == 'NoPressure'), 2)
+    for y in tf09_data[team]['Players']
+]
+high_press_rel_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['ReleasePressure'] == 'HighPressure'), 2)
+    for y in tf09_data[team]['Players']
+]
+low_press_rel_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['ReleasePressure'] == 'LowPressure'), 2)
+    for y in tf09_data[team]['Players']
+]
+no_press_rel_count = [
+    np.round(sum(1
+                 for x in y['SuccessfulPasses'] if x['ReleasePressure'] == 'NoPressure'), 2)
+    for y in tf09_data[team]['Players']
+]
+
